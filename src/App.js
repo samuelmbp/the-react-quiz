48232f4d5674f6e2ba8d7b875fa9cbab402875bd
Samuel Raducan <samuel.thembp@gmail.com>
2023-07-27 09:38:59 +0100

    Can start a quiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useReducer } from "react";
+import Main from "./components/Main";
 import Header from "./components/Header";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
-import Main from "./components/Main";
 import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
 
 const initialState = {
   questions: [],
@@ -15,6 +16,8 @@ const reducer = (state, action) => {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action unknown");
   }
@@ -38,7 +41,10 @@ const App = () => {
       <Main className="main">
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numOfQuestions={numOfQuestions} />}
+        {status === "ready" && (
+          <StartScreen numOfQuestions={numOfQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const StartScreen = ({ numOfQuestions }) => {
+const StartScreen = ({ numOfQuestions, dispatch }) => {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numOfQuestions} Questions to Test Your React Mastery</h3>
-      <button className="btn btn-ui">Let's Start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's Start
+      </button>
     </div>
   );
 };
